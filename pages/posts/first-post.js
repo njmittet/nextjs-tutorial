cd550077595b5dbb5cfbaadda04c544a6f07bb15
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-10-31 15:01:25 +0100

    Add example using classnames

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from '../../components/layout';
+import Alert from "../../components/Alert";
 
 export default function FirstPost() {
   return (
@@ -9,6 +10,9 @@ export default function FirstPost() {
           <title>First Post</title>
         </Head>
         <h1>First Post</h1>
+        <p>Testing Alert component</p>
+          <Alert type={"success"}>Success</Alert>
+          <Alert type={"error"}>Error</Alert>
       </Layout>
     </>
   );
