01e713d840ddf02fe86e1e91e52e9ecb10f9960c
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-11-02 18:36:45 +0100

    Add support for dynamic routing

---
+++ pages/posts/[id].js
@@ -0,0 +1,27 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// Pages that begin with [ and end with ] are dynamic routes in Next.js.
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+// Return a list of possible value for id.
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return { paths, fallback: false };
+}
+
+// Fetches data for the blog post with a provided id.
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return { props: { postData } };
+}
