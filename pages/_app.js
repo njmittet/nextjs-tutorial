f6650f124e2f9a3e6cc3b5f86db9f57988942a53
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-11-06 11:40:52 +0100

    Update comment inn _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // This App component is a top-level component which will be common across all the different pages.
 // Among other things, it can be used to to keep state when navigating between pages.
 
-// Global CSS can be imported from pages/_app.js only since the global CSS affects all elements on the page.
+// Global CSS can only be imported from this file since the global CSS affects all elements on the page.
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
