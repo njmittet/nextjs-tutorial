340aa4eb447883842946c6aed13679ccdf908ee4
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-11-06 12:09:27 +0100

    Updated dynamic routing comments

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,7 +4,8 @@ import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
 
-// Pages that begin with [ and end with ] are dynamic routes in Next.js.
+// Pages that begin with [ and end with ] are dynamic routes in Next.js, and can be extended to catch all paths by
+// adding three dots (...) inside the brackets.
 export default function Post({ postData }) {
   return (
     <Layout>
@@ -23,13 +24,21 @@ export default function Post({ postData }) {
   );
 }
 
-// Return a list of possible value for id.
+// Return a list of possible value for id. Must return an array as the value of the id key if the page is "catch-all":
+// return [
+//   {
+//     params: {
+//       // Statically Generates /posts/a/b/c
+//       id: ['a', 'b', 'c']
+//     }
+//   }
+// ]
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return { paths, fallback: false };
 }
 
-// Fetches data for the blog post with a provided id.
+// Fetches data for the blog post with a provided id. 'params.id' will be an array if the page is "catch-all":
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   return { props: { postData } };
